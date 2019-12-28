efa89d4e00b7ecff931c31f67408333c78f03c4c
Dan Langford <danlangford@ldschurch.org>
2019-12-28 08:53:38 -0700

    working DI jukebox prototype

---
+++ di.py
@@ -0,0 +1,3 @@
+from infinity import InfinityBase
+
+base = InfinityBase()
--- infinity/infinity.py
+++ infinity/infinity.py
@@ -1,29 +1,34 @@
 import threading
 from collections import defaultdict
-import hidapi
+import hid
+import tagids
+import tagurls
+import os
+import yaml
+import pafy
+import vlc
+
 
 class InfinityComms(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.device = self.initBase()
-	self.finish = False
+        self.device = self.init_base()
+        self.finish = False
         self.pending_requests = {}
         self.message_number = 0
         self.observers = []
 
-    def initBase(self):
-        hidapi.hid_init()
-        device = hidapi.hid_open(0x0e6f, 0x0129)
-	hidapi.hid_set_nonblocking(device, False)
+    def init_base(self):
+        device = hid.device()
+        device.open(0x0e6f, 0x0129)
+        device.set_nonblocking(0)
         return device
 
     def run(self):
         while not self.finish:
-            line = hidapi.hid_read_timeout(self.device,32,3000)
-
+            line = self.device.read(32, 3000)
             if not len(line):
                 continue
-
             fields = [c for c in line]
             if fields[0] == 0xaa:
                 length = fields[1]
@@ -35,14 +40,14 @@ class InfinityComms(threading.Thread):
                 else:
                     self.unknown_message(line)
             elif fields[0] == 0xab:
-                self.notifyObservers()
+                self.notify_observers()
             else:
                 self.unknown_message(line)
 
-    def addObserver(self, object):
-        self.observers.append(object)
+    def add_observer(self, obj):
+        self.observers.append(obj)
 
-    def notifyObservers(self):
+    def notify_observers(self):
         for obs in self.observers:
             obs.tagsUpdated()
 
@@ -53,11 +58,13 @@ class InfinityComms(threading.Thread):
         self.message_number = (self.message_number + 1) % 256
         return self.message_number
 
-    def send_message(self, command, data = []):
+    def send_message(self, command, data=[]):
         message_id, message = self.construct_message(command, data)
         result = Deferred()
         self.pending_requests[message_id] = result
-        hidapi.hid_write(self.device, message)
+        print("message")
+        print(message)
+        self.device.write(message)
         return Promise(result)
 
     def construct_message(self, command, data):
@@ -71,12 +78,12 @@ class InfinityComms(threading.Thread):
             message[index] = byte
             checksum = checksum + byte
         message[len(command_bytes)] = checksum & 0xff
-        return (message_id, map(chr, message))
+        return (message_id, message)
 
 
 class Deferred(object):
     def __init__(self):
-	self.event = threading.Event()
+        self.event = threading.Event()
         self.rejected = False
         self.result = None
 
@@ -115,7 +122,7 @@ class Promise(object):
 class InfinityBase(object):
     def __init__(self):
         self.comms = InfinityComms()
-        self.comms.addObserver(self)
+        self.comms.add_observer(self)
         self.onTagsChanged = None
 
     def connect(self):
@@ -140,15 +147,15 @@ class InfinityBase(object):
         def queryAllTags(idx):
             if len(idx) == 0:
                 then(dict())
-            numberToGet = [0] * len(idx)
-            tagByPlatform = defaultdict(list)
+            number_to_get = [0] * len(idx)
+            tag_by_platform = defaultdict(list)
             for (platform, tagIdx) in idx:
                 def fileTag(platform):
                     def inner(tag):
-                        tagByPlatform[platform].append(tag)
-                        numberToGet.pop()
-                        if len(numberToGet) == 0:
-                            then(dict(tagByPlatform))
+                        tag_by_platform[platform].append(tag)
+                        number_to_get.pop()
+                        if len(number_to_get) == 0:
+                            then(dict(tag_by_platform))
                     return inner
                 self.getTag(tagIdx, fileTag(platform))
         self.getTagIdx(queryAllTags)
@@ -171,29 +178,83 @@ class InfinityBase(object):
     def flashColor(self, platform, r, g, b):
         self.comms.send_message(0x93, [platform, 0x02, 0x02, 0x06, r, g, b])
 
+
 if __name__ == '__main__':
+
     import time
+    types = {'1': "Figure", '2': "Play Set", '3': "Game Disc", '4':"Power Disc, Ability", '5': "Power Disc, Toy", '6': "Power Disc, Customization"}
+    current_tags = set([])
+    data = yaml.load(file('data.yaml', 'r'), Loader=yaml.FullLoader)
+    Instance = vlc.Instance()
+    player = Instance.media_player_new()
 
-    def futurePrint(s):
-        print(s)
-
+    print("hello")
     base = InfinityBase()
 
-    base.onTagsChanged = lambda: futurePrint("Tags added or removed.")
+    def future_print(s):
+        print(s)
+
+    def tags_changed():
+        print("tags changed")
+        base.getAllTags(handle_tags)
+
+    def handle_tags(positions):
+        global current_tags
+        global data
+        global Instance
+        global player
+
+        data = yaml.load(file('data.yaml', 'r'), Loader=yaml.FullLoader)
+
+        base.flashColor(1, 200, 200, 200)
+
+        #print(positions)
+        all_tags = set('_'.join(map(str,tag)) for tags in positions.values() for tag in tags)
+        #print("current_tags", current_tags)
+        print("all_tags", all_tags)
+        for t in all_tags:
+            if not any(t == x['id'] for x in data['tags']):
+                desc = raw_input("Describe that:")
+                type = types[raw_input(str(types))]
+                data['tags'].append(dict({'id': t, 'desc': desc, 'type':type}))
+                yaml.dump(data, file('data.yaml', 'w'))
+
+        new_tags = all_tags - current_tags
+        print("new_tags",new_tags)
+        current_tags = all_tags
+        for new in new_tags:
+        #     os.system("open \""+tagurls.LISTING[tagids.LISTING[new]]+"\"")
+            player.stop()
+            x = next(item for item in data['tags'] if new == item['id'])
+            url = pafy.new(x['yt']).getbest().url
+            Media = Instance.media_new(url)
+            Media.get_mrl()
+            player.set_media(Media)
+            player.play()
+
+
+    base.onTagsChanged = tags_changed
+    #base.onTagsChanged = futurePrint
 
     base.connect()
+    print("connected")
 
-    base.getAllTags(futurePrint)
+    base.getAllTags(future_print)
 
-    base.setColor(1, 200, 0, 0)
+    print("colors")
+    base.setColor(1, 0, 0, 0)
+    base.setColor(2, 0, 0, 0)
+    base.setColor(3, 0, 0, 0)
 
-    base.setColor(2, 0, 56, 0)
-
+    base.fadeColor(1, 200, 0, 0)
+    base.fadeColor(2, 0, 200, 0)
     base.fadeColor(3, 0, 0, 200)
 
-    time.sleep(3)
+    time.sleep(1)
 
-    base.flashColor(3, 0, 0, 200)
+    base.flashColor(1, 0, 0, 0)
+    base.flashColor(2, 0, 0, 0)
+    base.flashColor(3, 0, 0, 0)
 
     print("Try adding and removing figures and discs to/from the base. CTRL-C to quit")
     while True:
